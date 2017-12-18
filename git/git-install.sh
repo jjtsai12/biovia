ac119505c87583df8e12c14efbb30fe0283bdfaf
Jeffrey Tsai <jeffrey.tsai@3ds.com>
2017-12-18 20:07:21 +0000

    updates

--- git/git-install.sh
+++ git/git-install.sh
@@ -10,6 +10,6 @@ fi
 # Delete old versions of git, on RHEL 7.4 it's 1.8.3 and there's no newer version in the RHEL repo
 yum -y erase git
 # Install a newer version of Git from ius
-curl -s https://raw.githubusercontent.com/iuscommunity/automation-examples/bash/enable-ius.sh | bash -
+curl -s https://raw.githubusercontent.com/jjtsai12/biovia/master/ius/enable-ius.sh | bash -
 yum -y install git2u
 git --version
